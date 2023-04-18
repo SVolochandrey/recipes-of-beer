44f618ee89a97498c3f076d2f522a2d36a0445f8
SVolochandrey <volochan.andrey@gmail.com>
2023-04-18 11:52:45 +0300

    hw-3-phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,6 +16,22 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+const contacts = localStorage.getItem('contacts');
+const parsedContacts = JSON.parse(contacts);
+
+if (parsedContacts) {
+this.setState({contacts: parsedContacts});
+}
+}
+
+componentDidUpdate(prevProps, prevState) {
+if (this.state.contacts !== prevState.contacts) {
+console.log('Renewed contacts');
+localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+}
+  }
+
   formSubmit = ({ name, number }) => {
     const contact = {
       id: nanoid(),
